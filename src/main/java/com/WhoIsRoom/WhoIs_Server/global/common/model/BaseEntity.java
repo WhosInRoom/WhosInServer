e0ced29c11824195580a88ba9dddda4823f77f88
Shinjongyun <tigerjong123@gmail.com>
2025-09-22 11:32:26 +0900

    [Feat] #1 BaseEntity

---
+++ src/main/java/com/WhoIsRoom/WhoIs_Server/global/common/model/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.WhoIsRoom.WhoIs_Server.global.common.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BaseStatus status;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.status == null) {
+            this.status = BaseStatus.ACTIVE;
+        }
+    }
+}
