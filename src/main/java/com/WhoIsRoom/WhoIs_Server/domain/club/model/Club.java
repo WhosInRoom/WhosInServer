558b47d55b73bc4142189615c8c03c234275923b
Shinjongyun <tigerjong123@gmail.com>
2025-09-22 11:39:15 +0900

    [Feat] #1 ClubEntity

---
+++ src/main/java/com/WhoIsRoom/WhoIs_Server/domain/club/model/Club.java
@@ -0,0 +1,30 @@
+package com.WhoIsRoom.WhoIs_Server.domain.club.model;
+
+import com.WhoIsRoom.WhoIs_Server.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "clubs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter @Getter
+public class Club extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", length = 200, nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "club_number", length = 100, nullable = false)
+    private String clubNumber;
+
+    @Builder
+    public Club(String name, String clubNumber) {
+        this.name = name;
+        this.clubNumber = clubNumber;
+    }
+
+}
