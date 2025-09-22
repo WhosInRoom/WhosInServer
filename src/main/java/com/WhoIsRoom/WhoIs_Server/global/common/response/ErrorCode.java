72e18035aee4bb65adaa2ad712b96693d29cbb40
Shinjongyun <tigerjong123@gmail.com>
2025-09-22 12:18:53 +0900

    [Feat] #1 ErrorCode

---
+++ src/main/java/com/WhoIsRoom/WhoIs_Server/global/common/response/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.WhoIsRoom.WhoIs_Server.global.common.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public enum ErrorCode{
+
+    // Common
+    ILLEGAL_ARGUMENT(100, BAD_REQUEST.value(), "잘못된 요청값입니다."),
+    NOT_FOUND(101, HttpStatus.NOT_FOUND.value(), "존재하지 않는 API 입니다."),
+    METHOD_NOT_ALLOWED(102, HttpStatus.METHOD_NOT_ALLOWED.value(), "유효하지 않은 Http 메서드입니다."),
+    SERVER_ERROR(103, INTERNAL_SERVER_ERROR.value(), "서버에 오류가 발생했습니다.");
+
+    private final int code;
+    private final int httpStatus;
+    private final String message;
+}
