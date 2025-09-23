ee829b00e663c062a07f79626ac7bef3af6a46dc
Shinjongyun <tigerjong123@gmail.com>
2025-09-23 16:44:29 +0900

    [Feat] #3 UserPrincipal

---
+++ src/main/java/com/WhoIsRoom/WhoIs_Server/domain/auth/model/UserPrincipal.java
@@ -0,0 +1,38 @@
+package com.WhoIsRoom.WhoIs_Server.domain.auth.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails{
+
+    private final Long userId;
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    /** UserDetails 구현 */
+    @Override
+    public String getPassword() { return password; }
+
+    @Override
+    public String getUsername() { return username; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
