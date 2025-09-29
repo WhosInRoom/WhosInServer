0841db5372e1e271a4ac25035b1cbe96195ee4b5
Shinjongyun <tigerjong123@gmail.com>
2025-09-29 11:01:32 +0900

    [Feat] #5 AuthController에 메일 api 추가

--- src/main/java/com/WhoIsRoom/WhoIs_Server/domain/auth/controller/AuthController.java
+++ src/main/java/com/WhoIsRoom/WhoIs_Server/domain/auth/controller/AuthController.java
@@ -1,15 +1,16 @@
 package com.WhoIsRoom.WhoIs_Server.domain.auth.controller;
 
+import com.WhoIsRoom.WhoIs_Server.domain.auth.dto.request.CodeCheckRequest;
+import com.WhoIsRoom.WhoIs_Server.domain.auth.dto.request.MailRequest;
 import com.WhoIsRoom.WhoIs_Server.domain.auth.service.JwtService;
+import com.WhoIsRoom.WhoIs_Server.domain.auth.service.MailService;
 import com.WhoIsRoom.WhoIs_Server.global.common.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final JwtService jwtService;
+    private final MailService mailService;
 
     @PostMapping("/logout")
     public BaseResponse<Void> logout(HttpServletRequest request){
@@ -28,6 +30,18 @@ public class AuthController {
     @PostMapping("/reissue")
     public BaseResponse<Void> reissueTokens(HttpServletRequest request, HttpServletResponse response) {
         jwtService.reissueTokens(request, response);
+        return BaseResponse.ok(null);
+    }
+
+    @PostMapping("/email/send")
+    public BaseResponse<Void> sendAuthCodeMail(@RequestBody MailRequest request) {
+        mailService.sendMail(request);
+        return BaseResponse.ok(null);
+    }
+
+    @PostMapping("/email/validation")
+    public BaseResponse<Void> checkAuthCode(@RequestBody CodeCheckRequest request) {
+        mailService.checkAuthCode(request);
         return BaseResponse.ok(null);
     }
 }
